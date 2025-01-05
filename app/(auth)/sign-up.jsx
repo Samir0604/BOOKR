949524fadf843f8e24706d2b9b660381faf207cd
Samir0604 <146883744+Samir0604@users.noreply.github.com>
2025-01-05 12:21:34 +0100

    bilder von google spotify etc

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,13 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, SafeAreaView, Image, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import Logo from "@/assets/logo/TemporaryBOOKRLogo.png"
 
-const signUp = () => {
+const LoginPage = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
   return (
-    <SafeAreaView>
-      <Text>signUp</Text>
+    <SafeAreaView className='bg-white'>
+        <View className='h-full flex-col items-center justify-center bg-white'>
+            <Image source={Logo}/>
+            <Text className='text-2xl color-[#8C8C8C] font-semibold'>Jetzt bei BOOKR registrieren</Text>
+
+
+            <View>
+              <View></View>
+            </View>
+       
+        </View>
     </SafeAreaView>
+    
   )
 }
 
-export default signUp
+export default LoginPage
