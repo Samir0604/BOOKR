ea4d8b1c4cba457cfeda0a83f8893d609b0e730b
Samir0604 <146883744+Samir0604@users.noreply.github.com>
2025-01-05 11:15:02 +0100

    link to onboarding

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,8 +2,9 @@ import { Slot, Stack } from "expo-router";
 import "../global.css"
 export default function RootLayout() {
     return (
-        <Stack>
+        <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="index" options={{ title: "Default App" }}></Stack.Screen>
+            <Stack.Screen name="(auth)" options={{ title: "auth" }}></Stack.Screen>
         </Stack>
     )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,14 @@
-import { Text } from "react-native";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
     return (
-        <Text className="font-bold text-5xl">Default App</Text>
+        <SafeAreaView>
+
+            <Link className="font-bold text-2xl" href="(auth)/onboarding">Onboarding</Link>
+
+        </SafeAreaView>
+
     )
 }
