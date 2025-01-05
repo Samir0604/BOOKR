e5c407f060854bf2eb3acf38fea3aaef0277336d
Samir0604 <146883744+Samir0604@users.noreply.github.com>
2025-01-05 11:27:43 +0100

    assets logo commit

--- app/(auth)/onboarding.jsx
+++ app/(auth)/onboarding.jsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const onboarding = () => {
   return (
-    <SafeAreaView>
-      <Text>onboarding</Text>
-    </SafeAreaView>
+    <View className='bg-green-500 flex-1'>
+
+      <View className='flex-1 items-center justify-center'>
+        <Image source={require('@/assets/logo/TemporaryBOOKRLogo.png')} className='size-50' />
+      </View>
+      
+      <View className='bg-white flex-1'>
+
+      </View>
+
+    </View>
   )
 }
 
