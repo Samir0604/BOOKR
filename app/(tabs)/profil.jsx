ffd5b66ad1546581cbc9ee66ac73f74938707424
Samir0604 <146883744+Samir0604@users.noreply.github.com>
2025-01-08 11:40:46 +0100

    Profile

--- app/(tabs)/profil.jsx
+++ app/(tabs)/profil.jsx
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
+import LinearGradientCard from '@/components/LinearGradientCard'
+import Logo from '@/assets/logo/TemporaryBOOKRLogo.png'
 const profil = () => {
   return (
-    <View>
-      <Text>profil</Text>
-    </View>
+    <ScrollView className='flex-1 bg-white'>
+    <SafeAreaView className='p-5 flex-1'>
+   
+
+        <View className='w-full items-center mb-8'>
+          <Image source={Logo} className='size-60' />
+          <Text className='font-bold text-black text-3xl text-center -mt-7'>Hallo Samir</Text>
+          <Text className='text-[#80BFAE] text-lg'>Mitglied seit 2024</Text>
+        </View>
+
+        <LinearGradientCard headline="Deine Sammlung" desc='Lese Bücher und erhalte Belohnungen' btnText='zur Sammlung' />
+
+        <View className='mt-8 bg-white'>
+          <Text className='text-black text-3xl font-bold'>Einstellungen</Text>
+
+          <View>
+            
+          </View>
+        </View>
+     
+    </SafeAreaView>
+    </ScrollView>
   )
 }
 
---
+++ components/LinearGradientCard.jsx
@@ -0,0 +1,49 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+import AiImage from '@/assets/images/ai.png'
+
+const LinearGradientCard = ({ headline, desc, btnText, ai = false, onPress }) => {
+  return (
+    <View
+    style={{
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 4 }, // nur nach unten
+      shadowOpacity: 0.3,
+      shadowRadius: 4,
+      elevation: 5, // für Android
+    }} // styles sind nur für den shadow weil dieser bei linear gradient nicht applied, der container selber ist aber der linear gradient!!!
+    >
+      <LinearGradient
+        colors={["#FFFFFF", "#6BB4A0"]}
+        style={{
+          width: '100%',
+          display: "flex",
+          flexDirection: 'column',
+          padding: 18,
+          gap: 30,
+          borderRadius: '5%',
+        }}
+      >
+        <View className='flex-col gap-2'>
+          <Text className='text-black font-bold text-3xl'>{headline}</Text>
+          <Text className='text-[#8C8C8C] text-lg'>{desc}</Text>
+        </View>
+        <View className='flex-row items-center justify-center'>
+          {ai ?
+          <TouchableOpacity className='bg-black w-7/12 px-4 py-3 rounded-full flex-row justify-between items-center'>
+          <Text className='text-white font-bold text-lg'>{btnText} </Text>
+          <Image source={AiImage} className='size-6' tintColor={"white"}/>
+        </TouchableOpacity>
+          :
+          <TouchableOpacity className='bg-black items-center justify-center w-7/12 py-3 rounded-full'>
+            <Text className='text-white font-bold text-lg'>{btnText}</Text>
+          </TouchableOpacity>}
+        </View>
+      </LinearGradient>
+    </View>
+
+  )
+}
+
+export default LinearGradientCard
