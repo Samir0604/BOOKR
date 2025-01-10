91792d78d6ad2a29b7dcabd2315cad31e6fee60f
Samir0604 <146883744+Samir0604@users.noreply.github.com>
2025-01-10 12:25:28 +0100

    hhh

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import BookProgress from '@/components/BookProgress'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
@@ -13,35 +13,36 @@ import Empfehlungen from '@/components/Empfehlungen';
 const home = () => {
 
 
-
   return (
 
     <ScrollView className='bg-white flex-1'>
 
-    
-    <SafeAreaView className='bg-white flex-1 p-4'>
-    
-              <View className="mt-5">
 
-              <View className='flex flex-row items-center border-b border-[#8C8C8C] '>
-              <Text className='text-4xl font-semibold'>Aktive Bücher</Text>
-              <MaterialIcons 
-                name="navigate-next" 
-                size={38} 
-                color="#2DA786"
-              />
-              </View>
+      <SafeAreaView className='bg-white flex-1 p-4'>
 
-                <BookProgress/>
-                <ZurSammlung />
-                <Empfehlungen />
-              </View>
-              
-           
-        
-       
-        </SafeAreaView>
-        </ScrollView>
+        <View className="mt-5">
+
+          <View className='flex flex-row items-center border-b border-[#8C8C8C] '>
+            <Text className='text-4xl font-semibold'>Aktive Bücher</Text>
+            <MaterialIcons
+              name="navigate-next"
+              size={38}
+              color="#2DA786"
+            />
+          </View>
+
+          <BookProgress />
+          <ZurSammlung />
+          <Empfehlungen />
+
+
+        </View>
+
+
+
+
+      </SafeAreaView>
+    </ScrollView>
   )
 }
 
--- components/Empfehlungen.jsx
+++ components/Empfehlungen.jsx
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import axios from 'axios';
 
-import BuchBeispiel from '@/assets/images/BuchBeispiel.png';
 
 
 export default function Empfehlungen() {
 
-    const books = [
-        { title: 'Lichtenstein', author: 'Simon Karper', image: BuchBeispiel },
-        { title: '1984', author: 'George Orwell', image: BuchBeispiel },
-        { title: '1984', author: 'George Orwell', image: BuchBeispiel },
-        { title: '1984', author: 'George Orwell', image: BuchBeispiel },
-        { title: '1984', author: 'George Orwell', image: BuchBeispiel },
-        { title: '1984', author: 'George Orwell', image: BuchBeispiel },
-        // Add more books as needed
-      ];
+  const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(false)
+  async function getBooks() {
+    try {
+      setLoading(true)
+      const res = await axios.get('https://www.googleapis.com/books/v1/volumes?q=intitle:1984+inauthor:George+Orwell&key=AIzaSyCARWlbz3FVwnVoD81zrscOZ4gPZ85jr40&maxResults=5')
+      setBooks(res.data.items)
+      setLoading(false)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
 
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, [])
 
 
 
   return (
-    <LinearGradient 
+    <LinearGradient
       colors={["rgba(107, 180, 160, 0.33)", "rgba(255, 255, 255, 0.44)"]}
       locations={[0.23, 0.77]}
       style={{
@@ -31,42 +40,73 @@ export default function Empfehlungen() {
       }}
     >
 
-        {/* oberster Container */}
-        <View className='p-4'>
+      {/* oberster Container */}
+      <View className='p-4'>
 
-            {/* Container für Überschrift und Text */}
-            <View className='flex-col gap-2'>
+        {/* Container für Überschrift und Text */}
+        <View className='flex-col gap-2'>
 
-                <View className='flex flex-row items-center'>
-                    <Text className='text-3xl font-bold'>Empfehlungen für dich </Text>
-                    <MaterialIcons 
-                    name="navigate-next" 
-                    size={38} 
-                    color="#2DA786"
-                     />
-                </View>
-                
-                <Text 
-                className='color-[#8C8C8C] font-medium text-lg'
-                style={{ lineHeight: 22 }}
-                >Für dich zusammengestellt: Lass dich von unserer Auswahl inspirieren</Text>
+          <View className='flex flex-row items-center'>
+            <Text className='text-3xl font-bold'>Empfehlungen für dich </Text>
+            <MaterialIcons
+              name="navigate-next"
+              size={38}
+              color="#2DA786"
+            />
+          </View>
 
-            </View>
-
-
-             {/* Bilder mit Titel rendern */}
-             <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} className='mt-4'>
-                {books.map((book, index) => (
-                <View key={index} className='mr-3'>
-                    <Image source={book.image} className='w-44 h-72 drop-shadow-md' />
-                    <Text className='font-bold text-lg'>{book.title}</Text>
-                    <Text className='color-[#8C8C8C]'>{book.author}</Text>
-                </View>
-          ))}
-             </ScrollView>
+          <Text
+            className='color-[#8C8C8C] font-medium text-lg'
+            style={{ lineHeight: 22 }}
+          >Für dich zusammengestellt: Lass dich von unserer Auswahl inspirieren</Text>
 
         </View>
-    
+
+
+        {/* Bilder mit Titel rendern */}
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} className='mt-4' >
+          {!loading ? books.map((book, index) => {
+            let title = book.volumeInfo.title
+            const thumbnail = `https://books.google.com/books/publisher/content/images/frontcover/${book.id}?fife=w400-h600&source=gbs_api`
+            let authors = book.volumeInfo.authors
+
+            return (
+              thumbnail != undefined && title != undefined && authors != undefined && (
+                <TouchableOpacity key={index} className='mr-3 flex-1'>
+                  <View className='w-44 h-72 drop-shadow-md mb-2 z-50' style={{
+                    shadowColor: 'black',
+                    shadowOffset: { width: 0, height: 6 },
+                    shadowOpacity: 0.5,
+                    shadowRadius: 5,
+                    elevation: 3,
+                  }}>
+                    <Image source={{ uri: thumbnail }} className='w-full h-full' resizeMode='stretch' />
+                  </View>
+                  <Text className="font-bold text-lg overflow-hidden">
+                    {title.length > 17 ? `${title.substring(0, 17)}..` : title}
+                  </Text>
+
+                  {
+                    authors.length === 1 ? (
+                      <Text className="color-[#8C8C8C]">{authors[0]}</Text>
+                    ) : authors.length === 2 ? (
+                      authors.map((author, index) => (
+                        <Text key={index} className="color-[#8C8C8C]">{author}</Text>
+                      ))
+                    ) : (
+                      authors.slice(0, 2).map((author, index) => (
+                        <Text key={index} className="color-[#8C8C8C]">{author}</Text>
+                      ))
+                    )
+                  }
+                </TouchableOpacity>)
+            )
+          }) : <ActivityIndicator size={'large'} color={'#2DA786'} style={{marginLeft: 165, marginTop: 10}} />}
+
+        </ScrollView>
+
+      </View>
+
     </LinearGradient>
   );
 }
