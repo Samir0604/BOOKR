275eb1c35647b284f0e7cad7edf58b4a49dc92ec
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-01-13 19:47:45 +0100

    bib push

---
+++ components/bookListingLib.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function bookListingLib(activeBook) {
+  return (
+    <View className="flex flex-col">
+
+      <View className="shadow-xl shadow-black/50">
+        <Image source={{uri: activeBook.image}} className='w-44 h-72'/>
+      </View>
+   
+      <Text className="mt-2 text-xl font-bold">{activeBook.title}</Text>
+      <Text className="color-[#8C8C8C]">{activeBook.authors}</Text>
+   
+    </View>
+  )
+}
