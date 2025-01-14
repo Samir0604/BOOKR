f853d9bf9faa222640c04d39ab3b7273e37017c5
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-01-14 13:48:46 +0100

    modal homepage

---
+++ components/editPorgressModal.jsx
@@ -0,0 +1,17 @@
+import { View, Text, Modal } from 'react-native'
+import React from 'react'
+
+export default function EditProgressModal({isOpen, children}) {
+  return (
+    <Modal
+    visible= {isOpen}
+    transparent
+    animationType='fade'
+    >
+        <View className='flex-1 items-center justify-end mb-[90px]'>
+            {children}
+        </View>
+
+    </Modal>
+  )
+}
